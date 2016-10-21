8b155849fdaca380489db0d158fe5e80ce4f2922
Marc Meszaros <me@marcmeszaros.com>
2016-10-20 18:37:55 -0700

    Simplify testing setup

--- setup.py
+++ setup.py
@@ -2,23 +2,11 @@
 # pylint: disable=C0111
 from __future__ import absolute_import
 import os
-import sys
 
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 
 from envitro import meta
 
-class ToxTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import tox
-        errcode = tox.cmdline(args=self.test_args)
-        sys.exit(errcode)
 
 # Get the long description from the relevant file
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -53,8 +41,7 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    tests_require=['tox'],
-    cmdclass={
-        'test': ToxTest
-    },
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest', 'mock'],
+    test_suite='tests',
 )
